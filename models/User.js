cf81986e5c371fde8b31ce0fad173c81a748a43f
김주형 <wnguddl96@knou.ac.kr>
2023-06-08 19:45:58 +0900

    fix(user): 이미 정의된 모델은 사용하고, 아닌 경우는 모델을 정의해주는 코드 작성을 통해 OverwriteModelError 해결

--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
     email: String,
-    password: String,
+    password: String
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 module.exports = User;
+
+
