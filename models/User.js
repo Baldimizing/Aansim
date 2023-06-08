1508c99abebb2fbc65ba6eb727ce127aeb0b82d0
김주형 <wnguddl96@knou.ac.kr>
2023-06-08 18:59:06 +0900

    refactor(User): 스키마 변경

--- models/User.js
+++ models/User.js
@@ -1,25 +1,11 @@
-const User = require('./models/User');
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
 
-// 사용자 생성
-const user = new User({
-    email: 'wnguddl96@naver.com',
-    password: '12345678'
+const userSchema = new Schema({
+    email: String,
+    password: String,
 });
 
-user.save((err) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('회원가입이 완료되었습니다.');
-    }
-});
+const User = mongoose.model('User', userSchema);
 
-// 사용자 조회
-User.find({}, (err,users) => {
-    if (err) {
-        console.error(err);
-    }else {
-        console.log(users);
-    }
-})
-
+module.exports = User;
