f704b2e9ac362d8740eb7b916a2a11b55d649586
김주형 <wnguddl96@knou.ac.kr>
2023-06-08 20:23:32 +0900

    refactor(UserController): 콜백 대신 promise 사용을 통한 MongooseError 해결 코드 추가

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,35 +1,33 @@
-const User = require('./models/User');
+const User = require('../models/user');
 
 // 회원가입
-const signup = (req, res) => {
+const signup = async (req, res) => {
   const { email, password } = req.body;
   const user = new User({
     email,
     password,
   });
 
-  user.save((err) => {
-    if (err) {
-      console.error(err);
+  try {
+    await user.save();
+    console.log('회원가입이 완료되었습니다.');
+    res.status(200).json({ message: '회원가입이 완료되었습니다.' });
+  } catch (err) {
+    console.error(err);
       res.status(500).json({ message: '회원가입에 실패했습니다.' });
-    } else {
-      console.log('회원가입이 완료되었습니다.');
-      res.status(200).json({ message: '회원가입이 완료되었습니다.' });
-    }
-  });
+  }
 };
 
 // 사용자 조회
-const getAllUsers = (req, res) => {
-  User.find({}, (err, users) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: '사용자 조회에 실패했습니다.' });
-    } else {
-      console.log(users);
-      res.status(200).json(users);
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        console.log(users);
+        res.status(200).json(users);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json( {message: '사용자 조회에 실패했습니다. '});
     }
-  });
 };
 
 module.exports = {
