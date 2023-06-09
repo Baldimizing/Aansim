4290e2761386fe3f3f3453a50ba7edabce1f4f42
김주형 <wnguddl96@knou.ac.kr>
2023-06-10 05:12:53 +0900

    refactor(userController): 핸들러에 일반 로그인 기능 추가

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,20 +1,22 @@
 const User = require('../models/user');
+const StatusCodes = require('http-status-codes');
+const bcrypt = require('bcrypt');
 
 // 회원가입
 const signup = async (req, res) => {
   const { email, password } = req.body;
   const user = new User({
     email,
-    password,
+    password: await bcrypt.hash(password, 20), // 비밀번호 해싱
   });
 
   try {
     await user.save();
     console.log('회원가입이 완료되었습니다.');
-    res.status(200).json({ message: '회원가입이 완료되었습니다.' });
+    res.status(StatusCodes.OK).json({ message: '회원가입이 완료되었습니다.' });
   } catch (err) {
     console.error(err);
-      res.status(500).json({ message: '회원가입에 실패했습니다.' });
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: '회원가입에 실패했습니다.' });
   }
 };
 
@@ -23,14 +25,39 @@ const getAllUsers = async (req, res) => {
     try {
         const users = await User.find({});
         console.log(users);
-        res.status(200).json(users);
+        res.status(StatusCodes.OK).json(users);
     } catch (err) {
         console.error(err);
-        res.status(500).json( {message: '사용자 조회에 실패했습니다. '});
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json( {message: '사용자 조회에 실패했습니다. '});
     }
 };
+
+// 로그인
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // 데이터베이스에서 해당 이메일을 조회하고 비밀번호 확인
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw new Error('등록되지 않았거나 잘못된 이메일이 입력되었습니다.');
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword) {
+            throw new Error('비밀번호가 일치하지 않습니다.');
+        }
+
+        res.status(StatusCodes.OK).json({ message: '로그인 성공'});
+    }catch (error) {
+        res.status(StatusCodes.UNAUTHORIZED).json({ message: error.message });
+    }
+}
 
 module.exports = {
   signup,
   getAllUsers,
+  login,
 };
