b90931b1f36d5a71d713007326e42e551d7c0ba9
김주형 <wnguddl96@knou.ac.kr>
2023-06-10 05:13:28 +0900

    refactor(userRoutes): 로그인 라우터 추가

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,4 +8,7 @@ router.post('/signup', UserController.signup);
 // 사용자 조회
 router.get('/', UserController.getAllUsers);
 
+// 로그인
+router.post('/login', UserController.login);
+
 module.exports = router;
