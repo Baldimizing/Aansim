315c05adf47a595a62efc51cf2c3ad684125a165
김주형 <wnguddl96@knou.ac.kr>
2023-06-10 05:12:13 +0900

    refactor(User): 착한 사람 점수 필드 스키마에 추가

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    score: {
+        type: Number,
+        default: 0
+    }
 });
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
