3ee626e0c0b7c7e4f619cc31f2e88bb6897a3772
김주형 <wnguddl96@knou.ac.kr>
2023-06-21 05:16:36 +0900

    refactor(api/signup): 사용자 입력 유효성 검증 추가와 모델 호출 경로 오타 수

--- api/signup.js
+++ api/signup.js
@@ -1,50 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const User = require('/..models.user');
+const { body, validationResult } = require('express-validator');
+const User = require('../models/user');
 const { StatusCodes } = require('http-status-codes');
-const { validationResult } = require('express-validator');
 
 // 회원가입 라우터
-router.post('/', async (req, res) => {
-
-    // 유효성 검증
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ errors: errors.array() });
-    }
-
-    const { email, password } = req.body;
-
+router.post(
+  '/',
+  [
+    body('email').isEmail().withMessage('올바른 이메일 주소를 입력하세요.'),
+    body('password').isLength({ min: 8 }).withMessage('비밀번호는 최소 8자 이상이어야 합니다.'),
+  ],
+  async (req, res) => {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors: errors.array() });
+      }
 
-        // 이메일 중복 확인
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ 
-                errors: [{ message: '이미 등록된 이메일입니다.' }],
-            });
-        }
+      const { email, password } = req.body;
 
-        // 비밀번호 해싱
-        const hashedPassword = await bcrypt.hash(password, 20);
+      // 이메일 중복 확인
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors: [{ message: '이미 등록된 이메일입니다.' }] });
+      }
 
-        // 새로운 사용자 생성
-        const initUser = new User({ 
-            email, password: hashedPassword 
-        });
+      // 비밀번호 해싱
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-        await initUser.save();
+      // 새로운 사용자 생성
+      const newUser = new User({
+        email,
+        password: hashedPassword,
+      });
 
-        res.status(StatusCodes.CREATED).json({ message: '회원가입이 완료되었습니다.' });
+      await newUser.save();
+
+      res.status(StatusCodes.CREATED).json({ message: '회원가입이 완료되었습니다.' });
     } catch (error) {
-        console.error(error);
-        res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ message: '회원가입에 실패했습니다.'});
+      console.error(error);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: '회원가입에 실패했습니다.' });
     }
-});
+  }
+);
 
 module.exports = router;
