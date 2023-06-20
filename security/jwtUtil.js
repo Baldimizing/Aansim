effc901a72231dc5a35ce6b523f2a5d468c459d9
김주형 <wnguddl96@knou.ac.kr>
2023-06-21 04:27:26 +0900

    refactor: expiresIn 오타 수

--- security/jwtUtil.js
+++ security/jwtUtil.js
@@ -3,20 +3,20 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const generateToken = (userId) => {
-    const token = jwt.sign({ userId }. process.env.JWT_SECRET, { enpiresIn: '1h' });
-    return token;
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  return token;
 };
 
 const verifyToken = (token) => {
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        return decoded;
-    } catch (error) {
-        throw new Error('유효하지 않은 토큰입니다.');
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded;
+  } catch (error) {
+    throw new Error('유효하지 않은 토큰입니다.');
+  }
 };
 
-module.exports = { 
-    generateToken, 
-    verifyToken,
- };
+module.exports = {
+  generateToken,
+  verifyToken,
+};
