83ae52fa88d988a160211b5d743c3b454fc7d7d1
김주형 <wnguddl96@knou.ac.kr>
2023-06-23 18:12:33 +0900

    feat(config): 사용자의 공통 설정 정보를 관리하는 config 추가

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    jwtSecret: 'your-secret-key',
+    database: {
+      host: 'localhost',
+      port: 27017,
+      name: 'aansim',
+      username: 'your-username',
+      password: 'your-password',
+    },
+    server: {
+      port: 3000,
+    },
+  };
+  
