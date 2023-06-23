2cdd1f85e8bf04386903e52d6daa0f0b59e12392
김주형 <wnguddl96@knou.ac.kr>
2023-06-23 18:25:41 +0900

    fix: validateBody가 check함수를 사용하도록 변경

--- security/requestValidation.js
+++ security/requestValidation.js
@@ -1,9 +1,11 @@
-const { body, validationResult } = require('express-validator');
+const { body, validationResult, check } = require('express-validator');
 const { StatusCodes } = require('http-status-codes');
 
 const validateBody = (fields) => {
   return [
     body(fields).exists().withMessage('필수 입력 필드입니다.'),
+    check(fields).isEmail().withMessage('올바른 이메일 주소를 입력하세요.'),
+    check(fields).notEmpty().withMessage('비밀번호를 입력하세요.'),
     (req, res, next) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
