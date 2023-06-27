fecd88ed9bd2c6cbcde9ba3f7401ff247899890b
김주형 <wnguddl96@knou.ac.kr>
2023-06-27 13:14:33 +0900

    refactor: welcome 페이지 디자인

--- aansim-client/src/components/Welcome.js
+++ aansim-client/src/components/Welcome.js
@@ -3,19 +3,21 @@ import './Welcome.css';
 
 function Welcome( {onStart }) {
     return (
-        <div className="welcome">
-            <header>
-                <h1>Try Aansim</h1>
-            </header>
-            <div className="welcom-body">
-                <p>직거래, 이제 안심하고 이용하세요</p>
-                <p>새로운 생태계를 만들어가보세요.</p>
-                <p>마음 편하게 거래할 수 있어요.</p>
-            </div>
-            <footer>
+        <div className="center">
+            <div className="outer-ring"></div>
+            <div className="inner-ring"></div>
+            <div className="welcome">
+                <header>
+                    <h2>직거래, 이젠 안심하세요</h2>
+                </header>
+                <div className="welcom-body">
+                    <p>새로운 생태계를 만들어가보세요.</p>
+                    <p>마음 편하게 거래할 수 있어요.</p>
+                </div>
                 <button onClick={onStart}>시작하기</button>
-            </footer>
+            </div>
         </div>
+        
     )
 }
 
