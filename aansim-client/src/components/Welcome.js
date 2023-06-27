1c19ebe5dd0a80679ddd6fd2dae7e318b0b41788
김주형 <wnguddl96@knou.ac.kr>
2023-06-27 13:56:57 +0900

    refacor: welcome page 세부 디자인 변경

--- aansim-client/src/components/Welcome.js
+++ aansim-client/src/components/Welcome.js
@@ -8,15 +8,17 @@ function Welcome( {onStart }) {
             <div className="inner-ring"></div>
             <div className="welcome">
                 <header>
-                    <h2>직거래, 이젠 안심하세요</h2>
+                    <h2 className="welcome-header">직거래, 이젠 안심하세요</h2>
                 </header>
-                <div className="welcom-body">
-                    <p>새로운 생태계를 만들어가보세요.</p>
-                    <p>마음 편하게 거래할 수 있어요.</p>
-                </div>
-                <button onClick={onStart}>시작하기</button>
+            <div className="welcome-body">
+                <p>새로운 생태계를 만들어가보세요.</p>
+                <p>마음 편하게 거래할 수 있어요.</p>
             </div>
-        </div>
+            <div className="btn-start">
+                <button onClick={onStart}>안심 가이드 시작하기</button>
+            </div>
+            </div>
+      </div>
         
     )
 }
