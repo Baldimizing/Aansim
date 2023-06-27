40f34ae55e84c9c36644b1863fca60b4328fb847
김주형 <wnguddl96@knou.ac.kr>
2023-06-28 03:07:43 +0900

    refactor: 페이지간 이동 관리를 쉽게 하기 위한 react-router-dom 활용위한 코드로 변경

--- aansim-client/src/App.js
+++ aansim-client/src/App.js
@@ -1,30 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useState } from 'react';
 import './App.css';
-import SignupLogin from './components/SignupLogin';
-import Welcome from './components/Welcome';
-import Quiz from './components/Quiz';
-
-
+import SignupLogin from './components/welcome/SignupLogin';
+import Welcome from './components/welcome/Welcome';
+import Quiz from './components/welcome/Quiz';
 
 function App() {
-  const [page, setPage] = useState('welcome');
   const [score, setScore] = useState(0); // 점수 관리용 생성자 추가
-  const [isSignUp, setIsSignUp] = useState(true); 
-
-  const startQuiz = () => {
-    setPage('quiz');
-  };
-
-  const showLogin = (quizScore) => {
-    setPage('login');
-    setScore(quizScore); // 퀴즈 끝난 점수 저장
-  };
+  const [isSignUp, setIsSignUp] = useState(true);
 
   return (
     <div className="app">
-      {page === 'welcome' && <Welcome onStart={startQuiz} />}
-      {page === 'quiz' && <Quiz onQuizEnd={showLogin} />}
-      {page === 'login' && <SignupLogin score={score} setScore={setScore} isSignUp={isSignUp} setIsSignUp={setIsSignUp} />}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/quiz" element={<Quiz onQuizEnd={setScore} />} />
+          <Route path="/login" element={<SignupLogin score={score} setScore={setScore} isSignUp={isSignUp} setIsSignUp={setIsSignUp} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
